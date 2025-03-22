38b15917a99be957edff11b7e8e662e83ed7ff08
andrew <dlrgus041@gmail.com>
2025-03-22 22:24:25 +0900

    complete main UI

--- game.lua
+++ game.lua
@@ -14,6 +14,10 @@ local uiGroup
 
 local background
 
+local scoreVarText
+local levelVarText
+local remainVarText
+
 local pauseBackgrond
 local pauseImg
 local pauseAt
@@ -52,7 +56,7 @@ local pos = {x = 0, y = 20}
 local piece = { 0,0, 0,0, 0,0, 0,0 }
 
 local box = {}
-local pocket = {}
+local bag = {}
 
 local gameBoardGroup
 local nextPieceGroup
@@ -68,7 +72,9 @@ local storedPieceId = 1
 local changePiece = false
 local limit = 30
 local remain = 1
-local maxScore = 10
+local maxLine = 10
+local line = 0
+local level = 1
 local score = 0
 
 local function scanBoard()
@@ -81,7 +87,12 @@ local function scanBoard()
 				break
 			end
 		end
-		if (isGameOver == true and isFull[col] == false) then isGameOver = false end
+		if (isFull[col] == true) then
+			score = score + 1
+			scoreVarText.text = score
+			line = line + 1
+			remainVarText.text = maxLine - line
+		elseif (isGameOver == true) then isGameOver = false end
 	end
 	return isGameOver
 end
@@ -89,15 +100,13 @@ end
 local function rearrangeBoard()
 	for bottom = 20, 1, -1 do
 		if (isFull[bottom] == true) then
-			for target = bottom - 1, 1, -1 do
-				if (isFull[target] == false) then
-					board[bottom] = board[target]
-					isFull[bottom] = false
-					board[target] = {8, 8, 8, 8, 8, 8, 8, 8, 8, 8}
-					isFull[target] = true
-					score = score + 1
-					break
-				end
+			local target = bottom - 1
+			while (target > 0 and isFull[target] == true) do target = target - 1 end
+			if (target > 0) then
+				board[bottom] = board[target]
+				isFull[bottom] = false
+				board[target] = {8, 8, 8, 8, 8, 8, 8, 8, 8, 8}
+				isFull[target] = true
 			end
 		end
 	end
@@ -128,15 +137,15 @@ end
 
 local function getRandomPiece()
 	paintNextPiece(true)
-	if (#pocket == 0) then
+	if (#bag == 0) then
 		for i = 1, 7 do
-			pocket[i] = i
+			bag[i] = i
 			box[i] = math.random(49)
 		end
-		table.sort(pocket, function(a, b) return box[a] > box[b] end)
+		table.sort(bag, function(a, b) return box[a] > box[b] end)
 	end
 	nowPieceId = nextPieceId
-	nextPieceId = table.remove(pocket)
+	nextPieceId = table.remove(bag)
 	paintNextPiece(false)
 end
 
@@ -146,33 +155,13 @@ local function setPiece(n)
 	end
 end
 
-local function getMin()
-	local ret = 0
-	for i = 1, 4 do ret = math.min(ret, piece[2 * i - 1]) end
-	return ret
-end
-
-local function getMax()
-	local ret = 0
-	for i = 1, 4 do ret = math.max(ret, piece[2 * i - 1]) end
-	return ret
-end
-
-local function predictBottom(nowPieceId)
-	local y0 = pos.y
-	while (y0 <= 20) do
-		local impossible = false
-		y0 = y0 + 1
-		for i = 1, 4 do
-			local y = y0 + piece[2 * i]
-			if (y > 20 or board[y][pos.x + piece[2 * i - 1]] < 8) then
-				impossible = true
-				break
-			end
-		end
-		if (impossible == true) then break end
+local function isPossible(dx, dy)
+	for i = 1, 4 do
+		local x0 = pos.x + dx + piece[2 * i - 1]
+		local y0 = pos.y + dy + piece[2 * i]
+		if (y0 > 20 or x0 < 1 or x0 > 10 or board[y0][x0] < 8) then return false end
 	end
-	return y0 - 1
+	return true
 end
 
 local function onKeyEvent(event)
@@ -181,8 +170,7 @@ local function onKeyEvent(event)
 		if (event.keyName == "escape") then
 			if (isGameOver == false) then
 				if (remain > 0) then
-					pauseAt = remain
-					remain = -1
+					pauseAt, remain = remain, -1
 					pauseBackgrond.isVisible = true
 					pauseImg.isVisible = true
 				else
@@ -191,7 +179,7 @@ local function onKeyEvent(event)
 					remain = pauseAt
 				end
 			end
-		elseif (event.keyName == "leftShift") then
+		elseif (event.keyName == "leftShift" or event.keyName == "c") then
 			if (changePiece == false) then
 				paintStoredPiece(true)
 				nowPieceId, storedPieceId = storedPieceId, nowPieceId
@@ -201,29 +189,39 @@ local function onKeyEvent(event)
 				paintStoredPiece(false)
 			end
 		elseif (event.keyName == "space") then
-			pos.y = predictBottom(nowPieceId)
-		elseif (event.keyName == "up") then
-			if (pos.y < predictBottom(nowPieceId)) then
-				for i = 1, 4 do piece[2 * i - 1], piece[2 * i] = -piece[2 * i], piece[2 * i - 1] end
+			local bottom = 0
+			while (isPossible(0, bottom + 1) == true) do bottom = bottom + 1 end
+			pos.y = pos.y + bottom
+		elseif (event.keyName == "up" or event.keyName == "x" or event.keyName == "numPad8") then
+			local left, right = 0, 0
+			for i = 1, 4 do
+				piece[2 * i - 1], piece[2 * i] = -piece[2 * i], piece[2 * i - 1]
+				left = math.min(left, piece[2 * i - 1])
+				right = math.max(right, piece[2 * i - 1])
 			end
-			local left, right = getMin(), getMax()
 			if (pos.x + left < 1) then
 				pos.x = pos.x - left
 			elseif (pos.x + right > 10) then
 				pos.x = pos.x - right
 			end
-		elseif (event.keyName == "down") then
-			if (pos.y < predictBottom(nowPieceId)) then
-				pos.y = pos.y + 1
+		elseif (event.keyName == "leftCtrl" or event.keyName == "z") then
+			local left, right = 0, 0
+			for i = 1, 4 do
+				piece[2 * i - 1], piece[2 * i] = piece[2 * i], -piece[2 * i - 1]
+				left = math.min(left, piece[2 * i - 1])
+				right = math.max(right, piece[2 * i - 1])
 			end
-		elseif (event.keyName == "left") then
-			if (pos.x + getMin() > 1) then
-				pos.x = pos.x - 1
+			if (pos.x + left < 1) then
+				pos.x = pos.x - left
+			elseif (pos.x + right > 10) then
+				pos.x = pos.x - right
 			end
-		elseif (event.keyName == "right") then
-			if (pos.x + getMax() < 10) then
-				pos.x = pos.x + 1
-			end
+		elseif (event.keyName == "down" or event.keyName == "numPad5") then
+			if (isPossible(0, 1) == true) then pos.y = pos.y + 1 end
+		elseif (event.keyName == "left" or event.keyName == "numPad4") then
+			if (isPossible(-1, 0) == true) then pos.x = pos.x - 1 end
+		elseif (event.keyName == "right" or event.keyName == "numPad6") then
+			if (isPossible(1, 0) == true) then pos.x = pos.x + 1 end
 		end
 		setPiece(nowPieceId)
 		paintBlocks()
@@ -236,7 +234,7 @@ local function onFrameEvent()
 
 	if (remain == 0) then
 		setPiece(8)
-		if (pos.y == predictBottom(nowPieceId)) then
+		if (isPossible(0, 1) == false) then
 			setPiece(nowPieceId)
 			changePiece = false
 			if (scanBoard() == false) then
@@ -251,9 +249,14 @@ local function onFrameEvent()
 			pos.y = pos.y + 1
 		end
 
-		if (score > maxScore) then
-			score = score - maxScore
-			maxScore = maxScore + 5
+		if (line >= maxLine) then
+			line = line - maxLine
+			maxLine = maxLine + 5
+			remainVarText.text = maxLine - line
+
+			level = level + 1
+			levelVarText.text = level
+
 			limit = limit - 2
 		end
 
@@ -282,13 +285,37 @@ function scene:create( event )
 	mainGroup = display.newGroup()
 	sceneGroup:insert(mainGroup)
 
-	local x0 = 3 * display.contentWidth / 4 + display.contentHeight / 8
+	local x1 = display.contentWidth / 4 - display.contentHeight / 8
+	local x2 = 3 * display.contentWidth / 4 + display.contentHeight / 8
 	local half = display.contentHeight / 40
 
-	nextPieceArea = display.newRoundedRect(mainGroup, x0, display.contentHeight / 4, 12 * half, 12 * half, 12)
+	local scoreBoardArea = display.newRoundedRect(mainGroup, x1, display.contentHeight / 2, 20 * half, 20 * half, 12)
+	scoreBoardArea:setFillColor(251 / 255, 206 / 255, 177 / 255, 0.8)
+	scoreBoardArea:setStrokeColor(1, 1, 1)
+	scoreBoardArea.strokeWidth = half / 2
+
+	scoreArea = display.newRoundedRect(mainGroup, x1 - 6 * half, display.contentHeight / 2 - 5 * half, 7 * half, 3 * half, 2)
+	scoreArea:setFillColor(203 / 255, 125 / 255, 96 / 255, 0.8)
+
+	scoreVarArea = display.newRoundedRect(mainGroup, x1 + 4 * half, display.contentHeight / 2 - 5 * half, 10 * half, 3 * half, 2)
+	scoreVarArea:setFillColor(203 / 255, 125 / 255, 96 / 255, 0.8)
+
+	levelArea = display.newRoundedRect(mainGroup, x1 - 6 * half, display.contentHeight / 2, 7 * half, 3 * half, 2)
+	levelArea:setFillColor(203 / 255, 125 / 255, 96 / 255, 0.8)
+
+	levelVarArea = display.newRoundedRect(mainGroup, x1 + 4 * half, display.contentHeight / 2, 10 * half, 3 * half, 2)
+	levelVarArea:setFillColor(203 / 255, 125 / 255, 96 / 255, 0.8)
+
+	remainArea = display.newRoundedRect(mainGroup, x1 - 6 * half, display.contentHeight / 2 + 5 * half, 7 * half, 3 * half, 2)
+	remainArea:setFillColor(203 / 255, 125 / 255, 96 / 255, 0.8)
+
+	remainVarArea = display.newRoundedRect(mainGroup, x1 + 4 * half, display.contentHeight / 2 + 5 * half, 10 * half, 3 * half, 2)
+	remainVarArea:setFillColor(203 / 255, 125 / 255, 96 / 255, 0.8)
+
+	nextPieceArea = display.newRoundedRect(mainGroup, x2, display.contentHeight / 4, 12 * half, 12 * half, 12)
 	nextPieceArea:setFillColor(1, 1, 1)
 
-	storedPieceArea = display.newRoundedRect(mainGroup, x0, 3 * display.contentHeight / 4, 12 * half, 12 * half, 12)
+	storedPieceArea = display.newRoundedRect(mainGroup, x2, 3 * display.contentHeight / 4, 12 * half, 12 * half, 12)
 	storedPieceArea:setFillColor(1, 1, 1)
 
 	gameBoardGroup = display.newGroup()
@@ -316,7 +343,7 @@ function scene:create( event )
 	for col = 1, 5 do
 		local line = display.newGroup()
 		for row = 1, 5 do
-			local grid = display.newRect(line, x0 - 5 * half + (2 * row - 1) * half, display.contentHeight / 4 - 5 * half + (2 * col - 1) * half, 2 * half, 2 * half)
+			local grid = display.newRect(line, x2 - 5 * half + (2 * row - 1) * half, display.contentHeight / 4 - 5 * half + (2 * col - 1) * half, 2 * half, 2 * half)
 			grid:setStrokeColor(0.5, 0.5, 0.5, 0.8)
 			grid.strokeWidth = half / 30
 		end
@@ -329,7 +356,7 @@ function scene:create( event )
 	for col = 1, 5 do
 		local line = display.newGroup()
 		for row = 1, 5 do
-			local grid = display.newRect(line, x0 - 5 * half + (2 * row - 1) * half, 3 * display.contentHeight / 4 - 5 * half + (2 * col - 1) * half, 2 * half, 2 * half)
+			local grid = display.newRect(line, x2 - 5 * half + (2 * row - 1) * half, 3 * display.contentHeight / 4 - 5 * half + (2 * col - 1) * half, 2 * half, 2 * half)
 			grid:setStrokeColor(0.5, 0.5, 0.5, 0.8)
 			grid.strokeWidth = half / 30
 		end
@@ -339,10 +366,79 @@ function scene:create( event )
 	uiGroup = display.newGroup()
 	sceneGroup:insert(uiGroup)
 
+	local scoreText = display.newText({
+		parent = uiGroup,
+		text = "LINES",
+		x = x1 - 6 * half,
+		y = display.contentHeight / 2 - 5 * half,
+		font = native.systemFont,
+		fontSize = 30,
+		align = "center"
+	})
+	scoreText:setFillColor(0, 0, 0)
+
+	scoreVarText = display.newText({
+		parent = uiGroup,
+		text = "0",
+		x = x1 + 4 * half,
+		y = display.contentHeight / 2 - 5 * half,
+		font = native.systemFont,
+		fontSize = 30,
+		align = "right",
+		width = 9 * half
+	})
+	scoreVarText:setFillColor(0, 0, 0)
+
+	local levelText = display.newText({
+		parent = uiGroup,
+		text = "LEVEL",
+		x = x1 - 6 * half,
+		y = display.contentHeight / 2,
+		font = native.systemFont,
+		fontSize = 30,
+		align = "center"
+	})
+	levelText:setFillColor(0, 0, 0)
+
+	levelVarText = display.newText({
+		parent = uiGroup,
+		text = "1",
+		x = x1 + 4 * half,
+		y = display.contentHeight / 2,
+		font = native.systemFont,
+		fontSize = 30,
+		align = "right",
+		width = 9 * half
+	})
+	levelVarText:setFillColor(0, 0, 0)
+
+	local remainText = display.newText({
+		parent = uiGroup,
+		text = "REMAIN",
+		x = x1 - 6 * half,
+		y = display.contentHeight / 2 + 5 * half,
+		font = native.systemFont,
+		fontSize = 30,
+		align = "center"
+	})
+	remainText:setFillColor(0, 0, 0)
+
+	remainVarText = display.newText({
+		parent = uiGroup,
+		text = "10",
+		x = x1 + 4 * half,
+		y = display.contentHeight / 2 + 5 * half,
+		font = native.systemFont,
+		fontSize = 30,
+		align = "right",
+		width = 9 * half
+	})
+	remainVarText:setFillColor(0, 0, 0)
+
 	local nextPieceText = display.newText({
 		parent = uiGroup,
 		text = "NEXT",
-		x = x0,
+		x = x2,
 		y = display.contentHeight / 4 - 8 * half,
 		font = native.systemFont,
 		fontSize = 50,
@@ -353,7 +449,7 @@ function scene:create( event )
 	local storedPieceText = display.newText({
 		parent = uiGroup,
 		text = "STORAGE",
-		x = x0,
+		x = x2,
 		y = 3 * display.contentHeight / 4 - 8 * half,
 		font = native.systemFont,
 		fontSize = 50,
